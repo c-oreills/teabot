d4051988e79df420788d3e4f32096d075a745fa0
Christy O'Reilly <christy@oreills.co.uk>
2013-10-11 11:54:11 +0000

    Add taring

--- weigh.py
+++ weigh.py
@@ -5,6 +5,7 @@ import spidev
 s = spidev.SpiDev()
 s.open(0, 0)
 
+tare_value = 0
 
 def weigh_raw():
     bytes = s.xfer2([1, 0, 0])
@@ -55,5 +56,14 @@ def window(seq, n):
         yield result
 
 
-def weigh():
+def weigh_untared():
     return conformant_moving_average(raw_weights())
+
+
+def weigh():
+    return weigh_untared() - tare_value
+
+
+def tare():
+    global tare_value
+    tare_value = weigh_untared()
